8caa1fb69dd82042eb9eba9e91ae0179a1392c08
PedroMakengo <pedromakengomiguel@gmail.com>
2022-10-16 19:24:15 +0000

    Adicionando e removendo os meus itens no meu localStorage melhorando o meu app

--- src/store/index.js
+++ src/store/index.js
@@ -10,13 +10,26 @@ export default createStore({
     loadProducts(state, products) {
       state.products = products;
     },
+    loadBag(state, products) {
+      state.productsInBag = products;
+    },
     addToBag(state, product) {
       state.productsInBag.push(product);
-      console.log(state.productsInBag.length);
+      // Trabalhando com o localStore adicionando os products selecionados
+      localStorage.setItem(
+        "productsInBag",
+        JSON.stringify(state.productsInBag)
+      );
     },
     removeFromBag(state, productId) {
       var updatedBag = state.productsInBag.filter(item => productId != item.id);
       state.productsInBag = updatedBag;
+
+      // Trabalhando com o localStore removendo os products selecionados
+      localStorage.setItem(
+        "productsInBag",
+        JSON.stringify(state.productsInBag)
+      );
     },
   },
   actions: {
@@ -24,6 +37,12 @@ export default createStore({
       axios.get("https://fakestoreapi.com/products").then(response => {
         commit("loadProducts", response.data);
       });
+    },
+
+    loadBag({ commit }) {
+      if (localStorage.getItem("productsInBag")) {
+        commit("loadBag", JSON.parse(localStorage.getItem("productsInBag")));
+      }
     },
 
     addToBag({ commit }, product) {
